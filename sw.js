5109216cacf732993c1383498b5373668bdeae72
funnypeter <funnypeter@gmail.com>
2026-03-29 11:33:50 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Drift Journal Service Worker v2
-const CACHE = 'drift-v6';  // bump version to force cache refresh
+const CACHE = 'drift-v8';  // bump version to force cache refresh
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
